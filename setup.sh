c1aa320dd90cb5d5100d0433cf2b99b9ce31bed8
Ilya Gorski <52842983+gorsil@users.noreply.github.com>
2019-07-28 03:53:26 +0300

    Separating link creation into functions and refactoring

--- setup.sh
+++ setup.sh
@@ -1,17 +1,31 @@
 #!/bin/zsh
-if [[ -d ~/.oh-my-zsh-d ]]; then
-	mv ~/.oh-my-zsh ~/.oh-my-zsh-old
-fi
 
-ln -fsr oh-my-zsh ~/
-mv ~/oh-my-zsh ~/.oh-my-zsh
+set -e
 
-ln -fsr zshrc ~/.zshrc
-ln -fsr vimrc ~/.vimrc
-ln -fsr xsessionrc ~/.xsessionrc
+function setup_link() {
+	ln -fsr $1 $2
+}
 
-ln -fsr zsh-syntax-highlighting ~/.oh-my-zsh/plugins
-ln -fsr zsh-autosuggestions ~/.oh-my-zsh/plugins
-ln -fsr zsh-256color ~/.oh-my-zsh/plugins
+function setup_dir_link() {
+	if [[ -d $2 ]] then
+		if [[ -h $2 ]] then
+			rm $2
+		else
+			mv $2 "${2}-old"
+		fi
+	fi
+	setup_link $1 $2
+}
 
-ln -fsr vim-lucius/colors/lucius.vim ~/.vim/colors
+setup_link zshrc ~/.zshrc
+setup_link vimrc ~/.vimrc
+setup_link xsessionrc ~/.xsessionrc
+
+setup_dir_link oh-my-zsh ~/.oh-my-zsh
+setup_dir_link zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+setup_dir_link zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+setup_dir_link zsh-256color ~/.oh-my-zsh/plugins/zsh-256color
+
+setup_link vim-lucius/colors/lucius.vim ~/.vim/colors
+
+unset -f setup_link setup_dir_link
